53811b0cb4a35a766b3ec9774ecbdb7c29d64263
Profirma <kiarafotos20@gmail.com>
2026-09-23 11:38:02 -0500

    Update procesar_pago.php

--- procesar_pago.php
+++ procesar_pago.php
@@ -1,4 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
 header('Content-Type: application/json');
 
 // Recibir los datos enviados desde el formulario web
